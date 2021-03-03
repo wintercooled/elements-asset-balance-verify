eacb61a942fafed3f61e8a8ff342d804af499cd0
wintercooled <matthew@blockstream.io>
2021-03-03 12:44:26 +0000

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,108 @@
 # elements-asset-balance-verify
-Searches Elements blocks for issueances, reissuances, burns of an asset to provide supply total
+Searches Elements blocks for issuances, reissuances, burns of an asset to provide supply total.
+
+Before running, set values for the following variables within the script:
+
+```
+ASSET_ID (optional, default None)
+RPC_USER
+RPC_PASSWORD
+RPC_PORT
+START_AT_BLOCK_HEIGHT (optional, default 0)
+```
+
+Your elements node must have txindex=1 in elements.conf. If it doesn't you need to add it to your confiog file and start elements with the ``--reindex`` argument once so it indexs the blockchain. After adding the config file setting and reindexing once, you can start elements normally from then on. If you do not do this the script will fail with an error: ``Error: No such mempool or blockchain transaction. Use gettransaction for wallet transactions.``.
+
+Example elements.conf for the live Liquid livuidv1 network (you must change the rpc* values below):
+
+```
+rpcuser=yourrpcusername
+rpcpassword=yourrpcpassword
+[liquidv1]
+rpcport=yourrpcport
+daemon=1
+server=1
+listen=1
+txindex=1
+fallbackfee=0.00000100
+```
+
+To install requirements and run the script:
+
+```
+virtualenv -p python3 venv
+
+source venv/bin/activate
+
+pip install python-bitcoinrpc
+
+python elements-asset-balance-verify.py
+```
+
+Example outputs using a local elementsregtest blockchain for demo purposes (any locally connected node will report the same details, regardless of which node issued, reissused or burned the asset):
+
+Output to file assets/b2e15d0d7a0c94e4e2ce0fe6e8691b9e451377f6e46e8045a86f7c4b5d4f0f23:
+
+```
+ISSUANCE (Unblinded)
+--------------------
+Asset Hex: b2e15d0d7a0c94e4e2ce0fe6e8691b9e451377f6e46e8045a86f7c4b5d4f0f23
+Token Hex: a6be6b365498cd451be75ba0f68c258ee01e08f3cb30d5f8469f6628db58dc61
+Asset Amount: 21000000.00000000
+Token Amount: 0
+Block: 0
+```
+
+Output to file assets/9b59712ae37518fcaa13cbaf3f136e17cd8968c4e0cba9e323e5970dcc18a5c3:
+
+```
+ISSUANCE (Unblinded)
+--------------------
+Asset Hex: 9b59712ae37518fcaa13cbaf3f136e17cd8968c4e0cba9e323e5970dcc18a5c3
+Token Hex: 7d6b3dbad4f68b61ecfc9a76657037b606b7262e9c253811a4cf0b0bf6aae2e9
+Asset Amount: 555.00000000
+Token Amount: 1.00000000
+Block: 5
+
+REISSUANCE (Unblinded)
+----------------------
+Asset Hex: 9b59712ae37518fcaa13cbaf3f136e17cd8968c4e0cba9e323e5970dcc18a5c3
+Asset Amount: 9.00000000
+Block: 6
+
+BURN
+----
+Asset Hex: 9b59712ae37518fcaa13cbaf3f136e17cd8968c4e0cba9e323e5970dcc18a5c3
+Amount: 7.00000000
+Block: 8
+```
+
+Output to file assets/0c28580034fbce24ad994950ed7df77ebcb9e78f4e8d795f5516f5d58012a6e7:
+
+```
+ISSUANCE (Blinded)
+------------------
+Asset Hex: 0c28580034fbce24ad994950ed7df77ebcb9e78f4e8d795f5516f5d58012a6e7
+Token Hex: 4681c7787707860b27ad55d585f09e3f4066fca4e9f2914e191dd489578391a7
+Asset Amount Commitment: 08e66b5681433cf5fbf16613185b5da6fbbbc9591047507b062c3c8e7d40c6ceb4
+Token Amount Commitment: 082fed56de34400ef22ee4ef32ccaca1bf54644954ec5e18ed57e00179add4854a
+Block: 4
+
+REISSUANCE (Blinded)
+--------------------
+Asset Hex: 0c28580034fbce24ad994950ed7df77ebcb9e78f4e8d795f5516f5d58012a6e7
+Asset Amount Commitment: 096df295348063d9b09cc2b5144f2a4a70599eb9c19c27bb5f3b6412d768cab41f
+Block: 7
+
+BURN
+----
+Asset Hex: 0c28580034fbce24ad994950ed7df77ebcb9e78f4e8d795f5516f5d58012a6e7
+Amount: 1.00000000
+Block: 9
+
+BURN
+----
+Asset Hex: 0c28580034fbce24ad994950ed7df77ebcb9e78f4e8d795f5516f5d58012a6e7
+Amount: 2.00000000
+Block: 10
+```
---
+++ elements-asset-balance-verify.py
@@ -0,0 +1,187 @@
+import json
+import os
+import time
+
+from bitcoinrpc.authproxy import AuthServiceProxy, JSONRPCException
+from pathlib import Path
+
+
+def main():
+
+    ###########################################################################
+    #              BEFORE RUNNING THE SCRIPT SET THE VALUES BELOW
+    ###########################################################################
+
+    # REQUIRED
+    # --------
+    # Elements RPC Credentials
+    # You need to change all 3 after setting them in your elements.conf file.
+    # See associated README for example config file format.
+    RPC_USER = 'yourusernamehere'
+    RPC_PASSWORD = 'yourpasswordhere'
+    RPC_PORT = 18884
+
+    # OPTIONAL
+    # --------
+    # Set the following to an asset hex if you want to report on a specific
+    # asset, otherwise leave it as None to report on all assets:
+    ASSET_ID = None
+
+    # OPTIONAL
+    # --------
+    # The script appends to file so if you run the script once, you can start
+    # again at the last block height processed. The last block processed will
+    # be saved in the file LAST_BLOCK after the script has run. 0 start from
+    # genesis:
+    START_AT_BLOCK_HEIGHT = 0
+
+    ###########################################################################
+    #              BEFORE RUNNING THE SCRIPT SET THE VALUES ABOVE
+    ###########################################################################
+
+    block_count = 0
+    block_hash = ''
+    rpc_ready = False
+    end_of_chain = False
+    block_height = START_AT_BLOCK_HEIGHT
+
+    # Delete asset files if we are starting from genesis:
+    if START_AT_BLOCK_HEIGHT == 0:
+        removeAssetFiles()
+
+    # Check node is ready for RPC calls
+    while not rpc_ready:
+        try:
+            rpc_connection = AuthServiceProxy(f'http://{RPC_USER}:{RPC_PASSWORD}@127.0.0.1:{RPC_PORT}')
+            block_count = rpc_connection.getblockcount()
+            rpc_ready = True
+        except Exception as e:
+            print(f'Cannot connect to node or node not ready for RPC: {e}')
+            print('Sleeping for 5 seconds...')
+            time.sleep(5)
+
+    print('Connected to node using RPC')
+
+    while not end_of_chain:
+        try:
+            print(f'Trying to process block at height {block_height}')
+            block_hash = rpc_connection.getblockhash(block_height)
+            block = rpc_connection.getblock(block_hash)
+            last_existing_block = block_height
+            txs = block['tx']
+
+            for tx in txs:
+                tx_hash = rpc_connection.getrawtransaction(tx)
+                tx_details = rpc_connection.decoderawtransaction(tx_hash)
+
+                #Issuances and Reissuances:
+                for vin in tx_details['vin']:
+                    issuance = vin.get('issuance')
+                    if issuance:
+                        if ASSET_ID is None or ASSET_ID == issuance['asset']:
+                            writeIssueOrReissue(issuance, block_height)
+                #Burns:
+                for vout in tx_details['vout']:
+                    vout_value = str(vout.get('value', '0E-8'))
+                    if vout_value != '0.00000000' and vout_value != '0E-8':
+                        script_pubkey = vout.get('scriptPubKey')
+                        if script_pubkey:
+                            asm = script_pubkey.get('asm')
+                            script_type = script_pubkey.get('type')
+                            if asm and script_type:
+                                if asm == 'OP_RETURN' and script_type == 'nulldata':
+                                    if ASSET_ID is None or ASSET_ID == vout['asset']:
+                                        writeBurn(vout, block_height)
+            block_height = block_height + 1
+        except Exception as e:
+            if hasattr(e, 'message'):
+                if e.message == 'Block height out of range':
+                    print(f'No block at height {block_height}. Reached chain tip. Stopping.')
+                else:
+                    print(f'Error: {e.message}')
+            else:
+                print(f'Error: {e}')
+            end_of_chain = True
+    print(f'Last block processed was at height {last_existing_block}. Saved last block height to "LAST_BLOCK" file.')
+    writeLastBlock(last_existing_block)
+
+def writeLastBlock(block_height):
+    with open(f'LAST_BLOCK', 'w') as filetowrite:
+        filetowrite.write(str(block_height))
+
+def writeBurn(burn, block_height):
+    Path('assets').mkdir(parents=True, exist_ok=True)
+    asset_hex = burn['asset']
+    with open(f'assets/{asset_hex}', 'a') as filetowrite:
+        amount = burn['value']
+        action = 'BURN'
+        filetowrite.write(f'{action}\n')
+        filetowrite.write('-' * len(action))
+        filetowrite.write('\n')
+        filetowrite.write(f'Asset Hex: {asset_hex}\n')
+        filetowrite.write(f'Amount: {amount}\n')
+        filetowrite.write(f'Block: {block_height}\n')
+        filetowrite.write('\n')
+
+def writeIssueOrReissue(issuance, block_height):
+    Path('assets').mkdir(parents=True, exist_ok=True)
+    asset_hex = issuance['asset']
+    with open(f'assets/{asset_hex}', 'a') as filetowrite:
+        asset_amount_type = 'Asset Amount'
+        token_amount_type = 'Token Amount'
+        token_amount = 0
+        issuance_type = None
+
+        token_hex = issuance.get('token')
+        is_reissuance = issuance['isreissuance']
+
+        # If there is no assetamount the issuance was blinded
+        asset_amount = issuance.get('assetamount')
+        if asset_amount:
+            issuance_type = 'Unblinded'
+        else:
+            asset_amount = issuance.get('assetamountcommitment')
+            if asset_amount:
+                asset_amount_type = 'Asset Amount Commitment'
+                issuance_type = 'Blinded'
+
+        if not is_reissuance:
+            # If there is no tokenamount the issuance was blinded
+            token_amount = issuance.get('tokenamount')
+            if not token_amount:
+                if asset_amount_type == 'Asset Amount Commitment':
+                    token_amount_type = 'Token Amount Commitment'
+                token_amount = issuance.get('tokenamountcommitment', 0)
+
+        if is_reissuance:
+            issuance_reissuance = 'REISSUANCE'
+        else:
+            issuance_reissuance = 'ISSUANCE'
+        filetowrite.write(f'{issuance_reissuance} ({issuance_type})\n')
+        filetowrite.write('-' * len(f'{issuance_reissuance} ({issuance_type})'))
+        filetowrite.write('\n')
+        filetowrite.write(f'Asset Hex: {asset_hex}\n')
+        if not is_reissuance:
+            filetowrite.write(f'Token Hex: {token_hex}\n')
+        filetowrite.write(f'{asset_amount_type}: {str(asset_amount)}\n')
+        if not is_reissuance:
+            if token_amount:
+                filetowrite.write(f'{token_amount_type}: {str(token_amount)}\n')
+            else:
+                filetowrite.write(f'{token_amount_type}: {0}\n')
+        filetowrite.write(f'Block: {block_height}\n')
+        filetowrite.write('\n')
+
+def removeAssetFiles():
+    folder = 'assets'
+    for filename in os.listdir(folder):
+        file_path = os.path.join(folder, filename)
+        try:
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)
+        except Exception as e:
+            print('Failed to delete %s. Reason: %s' % (file_path, e))
+
+
+if __name__ == '__main__':
+    main()
